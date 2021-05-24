b18262611cc6bde1469cf9645ebefe2585b9673a
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-05-25 01:08:13 +0900

    dexieを使うためにプロパティ登録

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+const db = chrome.extension.getBackgroundPage().db;
+window.db = db;
+
+Object.defineProperty(Vue.prototype, '$db', {
+	get() { return db; }
+});
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+	el: '#app',
+	render: h => h(App)
+});
